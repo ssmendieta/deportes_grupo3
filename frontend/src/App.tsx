8d1d02fc2975abf9f321fa40f901318905aa8441
Sergini34 <mendietasergio83gmail.com>
2026-04-22 12:44:56 -0400

    modificacion de ventanas de reserva

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,17 @@
-import React, { useState } from 'react';
-import ReservaForm from './components/reservas/ReservaForm';
-import AdminReserva from './components/reservas/AdminReserva';
+import React, { useState } from "react";
+import ReservaForm from "./components/reservas/ReservaForm";
+import AdminReserva from "./components/reservas/AdminReserva";
 
 const App = () => {
-  const [vista, setVista] = useState<'reserva' | 'admin'>('reserva');
+  const [vista, setVista] = useState<"reserva" | "admin">("admin");
 
   return (
     <div>
-      {/* ÚNICA NAVEGACIÓN */}
-      <div style={{ position: 'fixed', top: '20px', left: '20px', display: 'flex', gap: '10px', zIndex: 1000 }}>
-        <button 
-          onClick={() => setVista('reserva')}
-          style={{
-            padding: '8px 20px',
-            borderRadius: '20px',
-            border: 'none',
-            backgroundColor: vista === 'reserva' ? '#003366' : '#e0e0e0',
-            color: vista === 'reserva' ? 'white' : '#666',
-            fontWeight: 'bold',
-            cursor: 'pointer'
-          }}
-        >
-          Vista Usuario
-        </button>
-
-        <button 
-          onClick={() => setVista('admin')}
-          style={{
-            padding: '8px 20px',
-            borderRadius: '20px',
-            border: 'none',
-            backgroundColor: vista === 'admin' ? '#003366' : '#e0e0e0',
-            color: vista === 'admin' ? 'white' : '#666',
-            fontWeight: 'bold',
-            cursor: 'pointer'
-          }}
-        >
-          Vista Admin
-        </button>
-      </div>
-
-      {/* SOLO UNA VISTA */}
-      {vista === 'reserva' ? <ReservaForm /> : <AdminReserva />}
+      {vista === "reserva" ? (
+        <ReservaForm onVolverAdmin={() => setVista("admin")} />
+      ) : (
+        <AdminReserva onCrearReserva={() => setVista("reserva")} />
+      )}
     </div>
   );
 };
